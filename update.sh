0e664807428b416d9b6b45c13adb2a3c48dd9c36
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-05-05 22:58:26 +0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,16 @@
+#bin/bash
+clear
+echo
+echo "        Setting up first time installation just wait .....!"
+echo
+cd $HOME
+pkg install python -y
+pkg install python2 -y
+pkg install tor -y
+pkg install git -y
+pip install --upgrade pip
+pip3 install requests
+pip3 install bs4
+rm -rf tiktok
+
+git clone https://github.com/IRAQ-hacker/tiktok.git
