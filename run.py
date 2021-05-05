d1922df849d721ecd5112d7a05cf51043691c201
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-05-05 22:49:03 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,51 @@
+import os,sys,time
+
+os.system("clear")
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.08)
+
+jalan ("\033[1;91m===10%")
+os.system("clear")
+jalan ("\033[1;92m======40%")
+os.system("clear")
+jalan ("\033[1;93m===========60%")
+os.system("clear")
+jalan ("\033[1;94m===================80%")
+os.system("clear")
+jalan ("\033[1;95m==========================100%")
+os.system("clear")
+print("""                             
+          ,     ,
+         (\____/)
+          (_oo_)
+            (O)
+          __||__    \)
+       []/______\[] /
+       / \______/ \/
+      /    /__\
+     (\   /____\
+
+\033[1;91m1- github .
+\033[1;91m2- telegram account .
+\033[1;91m3- channel telegram .
+
+""")
+xp = input('type number : ')
+if xp =='1':         
+	print('please white second and Follow me in gitub for run the tool')
+	os.system('xdg-open https://github.com/iraq-hacker')
+	       
+if xp =='2':
+           
+    print('white second  please  ')
+    os.system('xdg-open https://t.me/iiwiw')
+           
+if xp =='3':
+           
+    print('white second  please and join in channel for run the toll ')
+    os.system('xdg-open https://t.me/professional_school')
+	
